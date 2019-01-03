d8a0c1b3bc9293bbb8233666b375bb0caa661dcc
Sourav Dutta <developersourav2012@gmail.com>
2019-01-03 23:45:04 +0530

    added python assignment1 code

---
+++ Assignment 1/main.py
@@ -0,0 +1,58 @@
+"""List down the attributes of my favorite song and print their values"""
+
+#Favorite Song Name
+song = "My Heart Will Go On"
+
+#Singer Name
+artist = "Celine Dion"
+
+#Song Writer Name
+writer = "James Horner"
+
+#Song Release Date
+releaseDate = "December 8, 1997"
+
+#Song Recorded Date
+recordedDate = "May 22, 1997"
+
+#Song Genre
+genre = "Pop"
+
+#Release Type
+releaseType = "Single"
+
+#Album Name
+albumName = "Let's Talk About Love"
+
+#Song Duration in Seconds
+durationInSeconds = 311
+
+#No of Copies Sold World Wide
+noOfCopiesSold = 18000000
+
+#Name of the awards won by this song
+awardsWon = "Academy Awards, Golden Globe Awards, Satellite Awards"
+
+#Describes the spectrum of emotions in music
+valence = "Happy to Sad"
+
+#Printing all the attributes
+print(song)
+print(artist)
+print(writer)
+print(releaseDate)
+print(recordedDate)
+print(genre)
+print(releaseType)
+print(albumName)
+print(durationInSeconds)
+print(noOfCopiesSold)
+print(awardsWon)
+print(valence)
+
+
+
+
+
+
+
